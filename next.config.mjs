999bf9efd338fa339ecdf803ba4cb48215766024
vietdq <vietdq@hblab.vn>
2024-07-02 14:25:51 +0700

    feat: i18n

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,7 @@
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin('./src/i18n/i18n.ts');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
@@ -5,4 +9,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
-import './globals.css';
+import '../globals.css';
 import React from 'react';
 import TanstackProvider from '@/contexts/TanstackProvider';
 
